c5302316f83d1171a0dcdeacd19171322ae38efa
kazuki.manabe <beesanzu@icloud.com>
2020-05-16 13:36:59 +0000

    tasklist-login-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks
   end
   
   def show
+    @task = Task.find(params[:id])
   end
   
   def new
@@ -27,6 +27,7 @@ class TasksController < ApplicationController
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
@@ -47,10 +48,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   def task_params
     params.require(:task).permit(:content, :status)
